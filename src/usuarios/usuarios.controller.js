ac2ec70258aef062e2b8dab41f7377f7e1b6896c
Skrexz <xsebastianrojasx@gmail.com>
2023-07-20 20:29:23 -0500

    Un poquito de estetica en el controller

--- src/usuarios/usuarios.controller.js
+++ src/usuarios/usuarios.controller.js
@@ -5,13 +5,15 @@ const {  findOne, find, create, update, remove } = require('./usuarios.service')
 usuarios.use(function timeLog(req, res, next){
     next();
 })
-usuarios.get('/',find);
-usuarios.get('/:id',findOne);
-usuarios.post('/',create);
-usuarios.put('/:id',update);
-usuarios.delete('/:id',remove);
 
-module.exports ={
+usuarios
+        .get('/',find)
+        .get('/:id',findOne)
+        .post('/',create)
+        .put('/:id',update)
+        .delete('/:id',remove);
+
+module.exports = {
     usuarios
 };
 
